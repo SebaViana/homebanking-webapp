9a281c3bf150d24fab17eefe826e6359f66e01b8
Sebastián Viana <vianasebastian2000@gmail.com>
2022-04-01 12:19:17 -0300

    html and css integration

--- homebanking/urls.py
+++ homebanking/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('homebanking/', include('login.urls')),
 ]
 
-urlpatterns += [
+""" urlpatterns += [
     path('login/', include('login.urls')),
 ]
 
@@ -34,4 +34,4 @@ urlpatterns += [
     
 ]
  
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) """
--- login/urls.py
+++ login/urls.py
@@ -1,5 +1,8 @@
+from django import views
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
+    path('login/', views.hello)
 
 ]
