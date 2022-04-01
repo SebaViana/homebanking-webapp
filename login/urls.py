232450f53617b823f1511045d0f0a2a404fd3d20
Sebastián Viana <vianasebastian2000@gmail.com>
2022-04-01 18:01:18 -0300

    added regisration url

--- login/urls.py
+++ login/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('login/', views.hello)
-
+    path('login/', views.hello),
+    path('register/', views.hella),
+    path('bank/', views.hellobank),
 ]
--- login/views.py
+++ login/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def hello(request):
-    return render(request, 'index.html')
+
+    return render(request, 'registration/login.html')
+
+def hella(request):
+
+    return render(request, 'registration/register.html')
+
+@login_required
+def hellobank(request):
+    return render(request, 'bank.html')
