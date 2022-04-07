436ab99752d74e6dc5ca1dae1cdd9b8d1f5410e4
Sebastián Viana <vianasebastian2000@gmail.com>
2022-04-06 22:00:03 -0300

    implemented user registration

--- login/views.py
+++ login/views.py
@@ -1,15 +1,24 @@
+from email import message
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 def hello(request):
 
     return render(request, 'registration/login.html')
 
 def hella(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RegisterForm()
 
-    return render(request, 'registration/register.html')
+    return render(request, 'registration/register.html', {'form':form})
 
 @login_required
 def hellobank(request):
