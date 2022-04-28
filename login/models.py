7d8a9714059438d55f1c77d94ca86fee9e555efb
Sebastián Viana <vianasebastian2000@gmail.com>
2022-04-28 19:32:08 -0300

    wallet model implemented MoneyField

--- login/models.py
+++ login/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from djmoney.models.fields import MoneyField
+
 import uuid
+
 
 class Wallet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    balance = models.IntegerField(default = 100)
+    balance = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
     accountId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) # to be modified
