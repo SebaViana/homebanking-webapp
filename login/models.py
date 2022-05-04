6f2f9653ba9fc4689ceaa48b411e2a827a866a05
Sebastián Viana <vianasebastian2000@gmail.com>
2022-05-04 20:35:50 -0300

    improved accountId - unique 6 digit

--- login/models.py
+++ login/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import User
 from djmoney.models.fields import MoneyField, Money
 
 import uuid
+import random
 
+def generateAccountId():
+    return str(random.randint(100000, 999999))
 
 class Wallet(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     balance = MoneyField(max_digits=14, decimal_places=2, default=Money(1000, "USD"))
-    accountId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) # to be modified
+    accountId = models.CharField(primary_key=True, default=generateAccountId, editable=False, max_length=6) # to be modified
