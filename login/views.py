033a2bc96a2a82c6deef86106249115f1602b495
SebaViana <vianasebastian2000@gmail.com>
2022-05-09 20:53:22 -0300

    implemented functional transfer view

--- login/views.py
+++ login/views.py
@@ -3,8 +3,13 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout, update_session_auth_hash
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
-from .forms import RegisterForm
+import decimal
+from .forms import RegisterForm, TransactionForm
 from .models import Wallet
+from djmoney.models.fields import MoneyField, Money
+import logging
+
+logging.basicConfig(level=logging.NOTSET)
 
 def hello(request):
     if request.user.is_authenticated:
@@ -65,6 +70,30 @@ def changePassword(request):
 
     return render(request, 'registration/change-password.html', context)
 
+""" def transfer(request):
+    user = Wallet.objects.get(user=request.user)
+    return render(request, 'transfer.html', {"balance":user.balance}) """
+
 def transfer(request):
     user = Wallet.objects.get(user=request.user)
-    return render(request, 'transfer.html', {"balance":user.balance})
+    if request.method == "POST":
+        form = TransactionForm(request.POST)
+        if form.is_valid():
+            sender = Wallet.objects.get(user=request.user)
+            if sender.balance.amount > Money(request.POST.get('amount')).amount:
+                trans = form.save()
+                trans.account_owner = request.user
+                sender.balance.amount -= int(request.POST.get('amount'))
+                sender.save()
+                receiver = Wallet.objects.get(accountId=request.POST.get('to_account'))
+                receiver.balance.amount += int(request.POST.get('amount'))
+                receiver.save()
+                return redirect ('transfer')
+
+    else:
+        form = TransactionForm
+    context= {
+    'TransactionForm_form': form,
+    "balance":user.balance
+    }
+    return render(request, "transfer.html", context)
