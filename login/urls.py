539989f83b5c2896dc94554dfb02ad1c68fb19cc
SebaViana <vianasebastian2000@gmail.com>
2022-05-09 22:12:47 -0300

    added password-reset and password-reset-confirm

--- login/urls.py
+++ login/urls.py
@@ -1,13 +1,19 @@
 from django import views
 from django.urls import include, path
 from . import views
+from django.contrib.auth import views as auth_views
+from login.views import ResetPasswordView
 
 urlpatterns = [
     path('login/', views.hello, name='login'),
     path('register/', views.hella, name='register'),
-    path('password-reset/', views.passwordReset, name='password-reset'),
+    #path('password-reset/', views.passwordReset, name='password-reset'),
     path('', views.hellobank, name='onlinebanking'),
     path('', views.closeSession, name='logout'),
     path('change-password/', views.changePassword, name = 'change-password'),
-    path('transfer', views.transfer, name="transfer")
+    path('transfer', views.transfer, name="transfer"),
+    path('password-reset/', ResetPasswordView.as_view(), name='password-reset'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='password-reset-confirm.html'),
+         name='password-reset-confirm'),
 ]
