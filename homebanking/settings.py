1cb2745002e51185aaca1a530c9ebd8cccdbd880
SebaViana <vianasebastian2000@gmail.com>
2022-05-09 22:37:22 -0300

    added email configs

--- homebanking/settings.py
+++ homebanking/settings.py
@@ -138,4 +138,12 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-SESSION_COOKIE_AGE = 5 * 60
+SESSION_COOKIE_AGE = 10 * 60
+
+# email configs
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = config("EMAIL_HOST")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
